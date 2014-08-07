cb0995c4ea52142e3e670ec3934c991ab2f609f1
yazeed44 <g--56@hotmail.com>
2014-08-07 03:15:19 +0300

    nothing works right now

---
+++ app/src/main/java/whitedessert/com/photosign/threads/BlendThread.java
@@ -0,0 +1,41 @@
+package whitedessert.com.photosign.threads;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import whitedessert.com.photosign.utils.BlendUtil;
+import whitedessert.com.photosign.utils.PhotoUtil;
+
+/**
+ * Created by yazeed44 on 8/7/14.
+ */
+public class BlendThread extends Thread {
+
+
+    private Activity a;
+    private String imgPath1,imgPath2;
+    private String pathBlended;
+    public BlendThread(String c,String s,Activity activity){
+this.imgPath1 = c;
+        this.imgPath2 = s;
+        this.a = activity;
+    }
+    @Override
+    public void run(){
+       Bitmap c = BitmapFactory.decodeFile(imgPath1), s = BitmapFactory.decodeFile(imgPath2);
+
+      Bitmap blended =  BlendUtil.combineImages(c,s);
+        pathBlended = PhotoUtil.saveBitmap(blended, a);
+        try {
+            this.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getPath(){
+        return pathBlended;
+    }
+}
---
+++ app/src/main/java/whitedessert/com/photosign/utils/PhotoLeftCorner.java
@@ -0,0 +1,21 @@
+package whitedessert.com.photosign.utils;
+
+/**
+ * Created by yazeed44 on 8/7/14.
+ */
+public final class PhotoLeftCorner {
+
+    private int x,y;
+
+    public PhotoLeftCorner(int x,int y){
+     this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+}
